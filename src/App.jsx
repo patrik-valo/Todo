52de1cb8a386dfc64dcd9f78aea1a4f087b51b5e
patrik-valo <patrik.saari1@outlook.com>
2026-09-01 11:32:41 +0300

    Todo osa 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
-import { useState } from 'react'
 import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Row from './components/Row'
+
+const apiUrl = 'http://localhost:3001'
 
 function App() {
   const [task, setTask] = useState('')
   const [tasks, setTasks] = useState([])
 
+  useEffect(() => {
+    axios.get(`${apiUrl}/tasks`)
+      .then(response => {
+        setTasks(response.data)
+      })
+      .catch(error => {
+        alert(error.response?.data?.message || error.message)
+      })
+  }, [])
+
   const addTask = (event) => {
     event.preventDefault()
 
-    const description = task.trim()
-    if (!description) return
+    const newTask = { description: task }
 
-    setTasks(currentTasks => [...currentTasks, description])
-    setTask('')
+    axios.post(`${apiUrl}/tasks`, { task: newTask })
+      .then(response => {
+        setTasks(currentTasks => [...currentTasks, response.data])
+        setTask('')
+      })
+      .catch(error => {
+        alert(error.response?.data?.error?.message || error.message)
+      })
   }
 
   const deleteTask = (deleted) => {
-    setTasks(currentTasks =>
-      currentTasks.filter(item => item !== deleted)
-    )
+    axios.delete(`${apiUrl}/tasks/${deleted}`)
+      .then(() => {
+        setTasks(currentTasks =>
+          currentTasks.filter(item => item.id !== deleted)
+        )
+      })
+      .catch(error => {
+        alert(error.response?.data?.error?.message || error.message)
+      })
   }
 
   return (
@@ -35,16 +60,12 @@ function App() {
 
       <ul>
         {
-          tasks.map(item => (
-            <li key={item}>
-              {item}
-              <button
-                className="delete-button"
-                onClick={() => deleteTask(item)}
-              >
-                Delete
-              </button>
-            </li>
+          tasks.map(task => (
+            <Row
+              task={task}
+              key={task.id}
+              onDelete={deleteTask}
+            />
           ))
         }
       </ul>

1250178d0fca49d4dbc595efa3275a27469f3d85
patrik-valo <patrik.saari1@outlook.com>
2026-09-03 15:44:25 +0300

    Todo osa 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import './App.css'
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { useUser } from './context/useUser'
 import axios from 'axios'
 import Row from './components/Row'
 
@@ -8,6 +8,7 @@ const apiUrl = 'http://localhost:3001'
 function App() {
   const [task, setTask] = useState('')
   const [tasks, setTasks] = useState([])
+  const { user } = useUser()
 
   useEffect(() => {
     axios.get(`${apiUrl}/tasks`)
@@ -22,27 +23,30 @@ function App() {
   const addTask = (event) => {
     event.preventDefault()
 
+    const headers = {headers: {Authorization: `Bearer ${user.token}`}}
     const newTask = { description: task }
 
-    axios.post(`${apiUrl}/tasks`, { task: newTask })
+    axios.post(`${apiUrl}/tasks`, { task: newTask }, headers)
       .then(response => {
         setTasks(currentTasks => [...currentTasks, response.data])
         setTask('')
       })
       .catch(error => {
-        alert(error.response?.data?.error?.message || error.message)
+        alert(error.response ? error.response.data.error.message : error)
       })
   }
 
   const deleteTask = (deleted) => {
-    axios.delete(`${apiUrl}/tasks/${deleted}`)
-      .then(() => {
+    const headers = {headers: {Authorization: `Bearer ${user.token}`}}
+
+    axios.delete(`${apiUrl}/tasks/${deleted}`, headers)
+      .then(response => {
         setTasks(currentTasks =>
           currentTasks.filter(item => item.id !== deleted)
         )
       })
       .catch(error => {
-        alert(error.response?.data?.error?.message || error.message)
+        alert(error.response ? error.response.data.error.message : error)
       })
   }
 
